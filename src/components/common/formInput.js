f0579990fbd64c219ca6f1e347b77c857862d534
Artur Grunau <artur.grunau@tum.de>
2015-12-24 01:06:00 +0000

    Add a FormInput component
    FormInput is a basic building block for form components. It wraps a
    Bootstrap form group combining a controlled input element with a label
    and a help block for easy error reporting.

---
+++ src/components/common/formInput.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const React = require('react');
+const _ = require('lodash');
+
+const FormInput = React.createClass({
+  propTypes: {
+    label: React.PropTypes.string.isRequired,
+    value: React.PropTypes.string.isRequired,
+    error: React.PropTypes.string,
+    onChange: React.PropTypes.func.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      inputId: _.uniqueId('formInput')
+    };
+  },
+
+  render: function() {
+    const wrapperClass = (this.props.error && this.props.error.length > 0) ? 'form-group has-error' : 'form-group';
+
+    return (
+      <div className={wrapperClass}>
+        <label className="control-label" htmlFor={this.state.inputId}>{this.props.label}</label>
+        <input
+          type="text"
+          className="form-control"
+          id={this.state.inputId}
+          value={this.props.value}
+          onChange={this.props.onChange} />
+        <span className="help-block">{this.props.error}</span>
+      </div>
+    );
+  }
+});
+
+module.exports = FormInput;
