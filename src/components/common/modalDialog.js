5750da9f5179629e6979618d1edb0700c85865e4
Artur Grunau <artur.grunau@tum.de>
2015-12-24 00:57:00 +0000

    Add a ModalDialog component
    ModalDialog wraps Bootstrap's modal and provides a reusable modal dialog
    component with the following features:
    - lazy rendering of body and footer elements
    - show() method that makes the dialog visible when invoked
    - onHide() prop invoked when the dialog closes

---
+++ src/components/common/modalDialog.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const React = require('react');
+const _ = require('lodash');
+
+const ModalDialog = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    children: React.PropTypes.node,
+    footer: React.PropTypes.node,
+    onHide: React.PropTypes.func
+  },
+
+  getInitialState: function() {
+    return {
+      visible: false,
+      labelId: _.uniqueId('modalDialogLabel')
+    };
+  },
+
+  componentDidMount: function() {
+    const dialogNode = $(this.refs.dialog.getDOMNode());
+
+    dialogNode.on('hidden.bs.modal', event => {
+      this.setState({visible: false});
+      this.props.onHide();
+      return false;
+    });
+  },
+
+  show: function() {
+    const dialogNode = $(this.refs.dialog.getDOMNode());
+
+    this.setState({visible: true});
+    dialogNode.modal();
+  },
+
+  render: function() {
+    const header = (
+      <div className="modal-header">
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span className="glyphicon glyphicon-remove"></span>
+        </button>
+        <h4 className="modal-title" id={this.state.labelId}>{this.props.title}</h4>
+      </div>
+    );
+
+    return (
+      <div className="modal fade" tabIndex="-1" role="dialog" aria-labelledby={this.state.labelId} ref="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            {header}
+            <div className="modal-body">
+              {this.state.visible ? this.props.children : null}
+            </div>
+            <div className="modal-footer">
+              {this.state.visible ? this.props.footer : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = ModalDialog;
